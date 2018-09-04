462d1383ae77ac486ca61f57a6df5bbb2d41baa7
jonidelv <delvallejonatan@gmail.com>
2018-09-04 15:39:37 +0700

    add redux devtools

--- store/index.js
+++ store/index.js
@@ -1,10 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import ReduxThunk from 'redux-thunk'
 import logger from 'redux-logger'
+import devTools from 'remote-redux-devtools'
 import rootReducer from '../reducers'
 
 const middlewares = [ReduxThunk, logger]
-const enhancer = [applyMiddleware(...middlewares)]
-const initialState = {}
 
-export default createStore(rootReducer, initialState, ...enhancer)
+export default createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(...middlewares),
+    devTools() // "Cmd+Ctrl+Arrow up" to activate
+  )
+)
